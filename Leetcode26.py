2e4f5c207c9ce5382b30f4911ae2a2bc5122fee8
YC-Cheng <cheng371@purdue.edu>
2021-07-12 09:31:49 -0500

    Update Leetcode26.py

--- Leetcode26.py
+++ Leetcode26.py
@@ -1,20 +1,16 @@
-def removeDuplicates(nums):
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
+#when looping a list try to use while loop
+#it does required to return the same length of list
 
-    i = 0
-    while i < len(nums)-1:
-        print(len(nums)-1)
-        if nums[i] == nums[i+1]:
-            del nums[i]
-        else:
-            i +=1
-    return len(nums) 
-
-nums = [1,1,2]
-removeDuplicates(nums)
-
-nums = [1,1,1,2,2,3,3,4,5,5]
-removeDuplicates(nums)
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0 #initialize index
+        while i < len(nums)-1: #make sure index won't goes above limit and it will updates even when item get deleted
+            if nums[i] == nums[i+1]:
+                del nums[i] #delete the item in original list
+            else:
+                i +=1   # if not the same will increase the index
+        return len(nums)
